ccc6f424dd3818ce8241a69c0a91e78773375758
amengodk <137509270+amengodk@users.noreply.github.com>
2023-06-26 08:03:26 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,118 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_20230623_123352_567",
+      "name": "IMG_20230623_123352_567",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.8787430645404726,
+        "pitch": -0.2583863043837411,
+        "fov": 1.3378744097411812
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.054147100054074,
+          "pitch": 0.23264500360603257,
+          "rotation": 0.7853981633974483,
+          "target": "1-img_20230623_123348_040"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-img_20230623_123348_040",
+      "name": "IMG_20230623_123348_040",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 3.0046661209558643,
+        "pitch": 0.1860246721409915,
+        "fov": 1.3378744097411812
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0046661209558643,
+          "pitch": 0.1860246721409915,
+          "rotation": 4.71238898038469,
+          "target": "2-img_20230623_123342_082"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-img_20230623_123342_082",
+      "name": "IMG_20230623_123342_082",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 3.0626485411259416,
+        "pitch": 0.154147493949349,
+        "fov": 1.3378744097411812
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
